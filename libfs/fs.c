c24d5e13e99d3b4228ca97e37e788fd78da390dc
RogerUC <122643899+RogerUC@users.noreply.github.com>
2023-03-04 15:26:54 -0800

    Tentative data structures

--- libfs/fs.c
+++ libfs/fs.c
@@ -8,6 +8,28 @@
 #include "fs.h"
 
 /* TODO: Phase 1 */
+typedef struct superBlock((__packed__)) 
+{
+  uint64_t signature;
+  uint16_t blockCount;
+  uint16_t rootDirIndex;
+  uint16_t dataBlockCount;
+  uint8_t  fatBlockCount;
+  //Unused/padding, 4079 bytes
+}
+
+typedef struct fat((__packed__)) 
+{
+  //N = Number of data blocks
+  uint16_t* fat = malloc(sizeof(*fat) * N);
+}
+
+typedef struct Root directory((__packed__)) 
+{
+  //The root directory is an array of 128 entries
+  uint32_t fat[FS_FILE_MAX_COUNT];
+  //Formatting of entries designated for functions
+}
 
 int fs_mount(const char *diskname)
 {
